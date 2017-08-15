d222329b6e403b465851979a007eebbfbb34e049
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 16:53:17 -0400

    Fix ADD_TODO typo.

--- src/actions/todoLists.js
+++ src/actions/todoLists.js
@@ -1,19 +1,27 @@
-const AddTodoList = todoList => {
-  type: 'CREATE_TODOLIST',
-  todoList
+export const addTodoList = (todoList) => {
+  return {
+    type: 'ADD_TODOLIST',
+    todoList
+  }
 }
 
-const EditTodoList = todoList => {
-  type: 'EDIT_TODOLIST',
-  todoList
+export const editTodoList = todoList => {
+  return {
+    type: 'EDIT_TODOLIST',
+    todoList
+  }
 }
 
-const FavoriteTodoList = id => {
-  type: 'FAVORITE_TODOLIST',
-  todoList
+export const favoriteTodoList = id => {
+  return {
+    type: 'FAVORITE_TODOLIST',
+    id
+  }
 }
 
-const DeleteTodoList = id => {
-  type: 'DELETE_TODOLIST',
-  todoList
+export const deleteTodoList = id => {
+  return {
+    type: 'DELETE_TODOLIST',
+    id
+  }
 }
