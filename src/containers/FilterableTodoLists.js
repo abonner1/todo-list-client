f252a7ef19cb99ea9c038226bcee37d42d93e266
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 16:52:47 -0400

    Map Dispatch to Props for FilterableTodoLists.

--- src/containers/FilterableTodoLists.js
+++ src/containers/FilterableTodoLists.js
@@ -1,12 +1,21 @@
 import { connect } from 'react-redux'
-import TodoLists from '../components/TodoLists';
+import TodoLists from '../components/TodoLists'
 
 const mapStateToProps = state => {
   return {todoLists: state.todoLists}
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    onTodoListSubmit: action => {
+      dispatch(action)
+    }
+  }
+}
+
 const FilterableTodoLists = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(TodoLists)
 
 export default FilterableTodoLists;
