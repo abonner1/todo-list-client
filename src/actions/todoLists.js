dd3f61553c19f0aff7bdf8d3ad7bbd08ff51bcda
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 11:29:37 -0400

    Add Favorites Action for TodoLists.

--- src/actions/todoLists.js
+++ src/actions/todoLists.js
@@ -8,6 +8,11 @@ const EditTodoList = todoList => {
   todoList
 }
 
+const FavoriteTodoList = todoList => {
+  type: 'FAVORITE_TODOLIST',
+  todoList
+}
+
 const DeleteTodoList = todoList => {
   type: 'DELETE_TODOLIST',
   todoList
