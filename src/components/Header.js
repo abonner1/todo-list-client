4e07f7297e8074655473262328a99bb412bf471a
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 10:52:04 -0400

    Stub out Header.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        Todo List App Banner
+      </div>
+    )
+  }
+}
+
+export default Header;
