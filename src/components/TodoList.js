4b34288d0c37179cab3f806b236fe91b2999c923
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 16:51:20 -0400

    Refactor TodoList Component.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TodoList = ({ todoList }) => {
   return (
-    <li>{todoList.id} - {todoList.name}</li>
+    <li>{todoList.name} <button>X</button></li>
   )
 }
 
