252ef21864f87165d53452d14ea7578b00adeb40
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 17:17:09 -0400

    Connect VisibleTodoList Container.

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
+import { connect } from 'react-redux'
 import TodoList from '../components/TodoList'
 
-class VisibleTodoList extends Component {
-  render() {
-    return (
-      <TodoList />
-    )
+const mapStateToProps = state => {
+  return {
+    todoLists: state.todoLists,
+    todos: state.todos
   }
 }
+
+const VisibleTodoList = connect(mapStateToProps)(TodoList)
 
 export default VisibleTodoList;
