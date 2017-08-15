c3be324cba7ec3ea5cbc4326ab61801448adc754
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 17:01:23 -0400

    Add presentational Components from VisibleTodoList.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import AddTodo from './AddTodo';
 
-const TodoList = ({ todoList }) => {
+const TodoList = (props) => {
   return (
-    <li>{todoList.name} <button>X</button></li>
+    <div>
+      <h1>Current Todo List</h1>
+      <AddTodo />
+      <ul>
+        <li><input type="checkbox" /> Milk <button>X</button></li>
+        <li><input type="checkbox" /> Eggs <button>X</button></li>
+        <li><input type="checkbox" /> Butter <button>X</button></li>
+      </ul>
+    </div>
   )
 }
 
