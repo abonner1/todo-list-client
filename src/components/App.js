cd9bc060621c3b76546ae306f9be23710f9c8770
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 11:59:11 -0400

    Import Components to App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import DayPicker from './CalendarWidget';
+import FilterableTodoLists from '../containers/FilterableTodoLists';
+import Header from './Header';
+import VisibleTodoList from './VisibleTodoList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello world!
+        <Header />
+        <DayPicker />
+        <FilterableTodoLists />
+        <VisibleTodoList />
       </div>
     );
   }
