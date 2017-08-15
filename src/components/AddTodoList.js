18be15c9d646cc18807c548924ad394387ea2bb1
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 12:49:37 -0400

    Transform AddTodoList button to input on click.

--- src/components/AddTodoList.js
+++ src/components/AddTodoList.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddTodoList = props => {
-  return (
-    <a href="#">+ Add Todo List</a>
-  )
+class AddTodoList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showAddBox: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      showAddBox: true
+    })
+  }
+
+  render () {
+    const AddTodoListBox =  this.state.showAddBox ? <input type="text" /> : <button onClick={this.handleClick}>+ Add Todo</button>
+    return (
+      <div>
+        {AddTodoListBox}
+      </div>
+    )
+  }
 }
 
 export default AddTodoList;
