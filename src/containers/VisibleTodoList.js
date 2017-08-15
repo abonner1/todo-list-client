42c5e10a731b9a702c76e4a6550fe664563e473d
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 17:01:01 -0400

    Move presentational elements to TodoList Component.

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,19 +1,11 @@
 import React, {Component} from 'react';
-import AddTodo from '../components/AddTodo';
+import TodoList from '../components/TodoList'
 
 class VisibleTodoList extends Component {
   render() {
     return (
-      <div>
-        <h1>Current Todo List</h1>
-        <AddTodo />
-        <ul>
-          <li><input type="checkbox" /> Milk <button>X</button></li>
-          <li><input type="checkbox" /> Eggs <button>X</button></li>
-          <li><input type="checkbox" /> Butter <button>X</button></li>
-        </ul>
-      </div>
-  )
+      <TodoList />
+    )
   }
 }
 
