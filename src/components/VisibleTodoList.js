219042afc8d4e2bb1d88a5db2640ac6b4422f1b7
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 11:00:09 -0400

    Stub CurrentTodoList view.

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import AddTodo from './AddTodo';
 
 class VisibleTodoList extends Component {
   render() {
     return (
       <div>
         <h1>Current Todo List</h1>
+        <AddTodo />
         <ul>
           <li>Milk</li>
           <li>Eggs</li>
