768a982ba79ca02eb2ab9d775fa299ef95ccaa75
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 16:58:42 -0400

    Move list item to TodoLists.

--- src/components/TodoLists.js
+++ src/components/TodoLists.js
@@ -3,7 +3,7 @@ import TodoList from './TodoList';
 import AddTodoList from './AddTodoList';
 
 const TodoLists = props => {
-  const todoLists = props.todoLists.map((todoList, i) => <TodoList key={i} todoList={todoList} />)
+  const todoLists = props.todoLists.map((todoList, i) => <li key={i}>{todoList.name}</li>)
 
   return (
     <div>
