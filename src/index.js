3e6127fb274760657ed91c6abe7cdd0aeb8a88de
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-15 12:24:28 -0400

    Initialize Redux store.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import App from './components/App'
+import TodoApp from './reducers/index'
+import registerServiceWorker from './registerServiceWorker'
+
+const store = createStore(TodoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
