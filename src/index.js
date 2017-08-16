0a3cb646b246718b60ed73882c9265c508363677
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-16 10:25:09 -0400

    Add Router to Index.js.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import App from './components/App'
 import TodoApp from './reducers/index'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
 const store = createStore(TodoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route exact path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
