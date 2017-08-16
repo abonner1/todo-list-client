dd7f11f2f509244d793787aa91e71d63e3120ff8
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-16 13:02:28 -0400

    Add ADD_TODO action type to todos reducer.

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,5 +1,9 @@
+let i = 1
 const todos = (state = [], action) => {
   switch(action.type) {
+    case 'ADD_TODO':
+      const newTodo = {...action.todo, id: i++}
+      return [...state, newTodo]
     default:
       return state
   }
