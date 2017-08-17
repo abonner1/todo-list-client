0d369b0e423ccb9f464c40d8586755ef1442f470
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-17 16:23:06 -0400

    Remove Redirect temporarily.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import App from './containers/App'
 import TodoApp from './reducers/index'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
 const store = createStore(TodoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
@@ -13,8 +13,7 @@ ReactDOM.render(
   <Provider store={store}>
     <Router>
       <div>
-        <Route path="/todo_lists" component={App} />
-        <Redirect from="/" to="todo_lists" />
+        <App />
       </div>
     </Router>
   </Provider>,
