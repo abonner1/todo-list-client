143e0c2e52838b252a117680b13b39c9cf711cea
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-17 16:23:43 -0400

    Add '/sign_up' and '/sign_in' routes to App Component.

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,8 @@ import ConnectedTodoLists from './ConnectedTodoLists'
 import Header from '../components/Header'
 import VisibleTodoList from './VisibleTodoList'
 import { Route } from 'react-router-dom'
+import SignIn from '../components/User/SignIn'
+import SignUp from '../components/User/SignUp'
 
 class App extends Component {
   render() {
@@ -12,6 +14,8 @@ class App extends Component {
         <Route key="0" path="/todo_lists" exact component={ConnectedTodoLists} />
         <Route key="1" path="/todo_lists/:todo_list_id" exact component={ConnectedTodoLists} />
         <Route key="2" path="/todo_lists/:todo_list_id" exact component={VisibleTodoList} />
+        <Route key="3" path="/sign_up" exact component={SignUp} />
+        <Route key="4" path="/sign_in" component={SignIn} />
       </div>
     );
   }
