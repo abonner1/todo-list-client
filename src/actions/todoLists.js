bad3b1fdea1847475843b01660903b9650c2e3a3
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-17 18:07:49 -0400

    Remove unused actions.

--- src/actions/todoLists.js
+++ src/actions/todoLists.js
@@ -5,20 +5,6 @@ export const addTodoList = (todoList) => {
   }
 }
 
-export const editTodoList = todoList => {
-  return {
-    type: 'EDIT_TODOLIST',
-    todoList
-  }
-}
-
-export const favoriteTodoList = id => {
-  return {
-    type: 'FAVORITE_TODOLIST',
-    id
-  }
-}
-
 export const deleteTodoList = id => {
   return {
     type: 'DELETE_TODOLIST',
