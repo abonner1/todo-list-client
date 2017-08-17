0d12a9893e806ac3445326944e4efa64ae0f3943
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-17 18:00:04 -0400

    Remove Header.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ConnectedTodoLists from './ConnectedTodoLists'
-import Header from '../components/Header'
+// import Header from '../components/Header'
 import VisibleTodoList from './VisibleTodoList'
 import { Route } from 'react-router-dom'
 import SignIn from '../components/User/SignIn'
@@ -10,7 +10,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
         <Route key="0" path="/todo_lists" exact component={ConnectedTodoLists} />
         <Route key="1" path="/todo_lists/:todo_list_id" exact component={ConnectedTodoLists} />
         <Route key="2" path="/todo_lists/:todo_list_id" exact component={VisibleTodoList} />
