6bcec163934baf803826c3e89de29fa390e1dcc9
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-17 18:07:21 -0400

    Handle Sign up form changes.

--- src/components/User/SignUp.js
+++ src/components/User/SignUp.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Sign up</h1>
         <form>
-          <input type="text" placeholder="Your Email" />
-          <input type="password" name="password" placeholder="Password"/>
-          <input type="password" name="passwordConfirmation" placeholder="Confirm Password" />
+          <input type="text" placeholder="Your Email" name="email" onChange={this.handleChange} value={this.state.email} />
+          <input type="password" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password}/>
+          <input type="password" name="passwordConfirmation" placeholder="Confirm Password" onChange={this.handleChange} value={this.state.passwordConfirmation} />
           <input type="submit" value="Sign up" />
         </form>
       </div>
