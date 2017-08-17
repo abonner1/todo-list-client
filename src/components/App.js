9f1fd1af00eb6179019fbc422b78d6ca40465a27
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-16 21:38:18 -0400

    Refactor Routes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,17 @@
 import React, { Component } from 'react'
-import DayPicker from './CalendarWidget'
 import FilterableTodoLists from '../containers/FilterableTodoLists'
 import Header from './Header'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import { Route } from 'react-router-dom'
-
-const routes = [
-  {
-    path: "/todo_lists",
-    exact: true
-  },
-  {
-    path: "/todo_lists/:todo_list_id",
-    exact: true
-  }
-]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-          <div>
-            {routes.map((route, index) => (
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={DayPicker}
-              />
-            ))}
-          </div>
-          <div>
-            {routes.map((route, index) => (
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={FilterableTodoLists}
-              />
-            ))}
-          </div>
-          <div>
-            <Route
-              path={routes[1].path}
-              exact={routes[1].exact}
-              component={VisibleTodoList}
-            />
-          </div>
+          <Route key="0" path="/todo_lists" exact component={FilterableTodoLists} />
+          <Route key="1" path="/todo_lists/:todo_list_id" exact component={FilterableTodoLists} />
+          <Route key="2" path="/todo_lists/:todo_list_id" exact component={VisibleTodoList} />
       </div>
     );
   }
