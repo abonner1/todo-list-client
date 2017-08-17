d9e1266b8a6a45fd8e158888c2d76adc3b4f3f1f
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-17 19:53:40 -0400

    Refactor.

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,41 +1,25 @@
 import fetch from 'isomorphic-fetch'
 
-export const addUser = () => {
-  return {
-    type: 'ADD_USER'
-  }
-}
+export const addUser = () => ({ type: 'ADD_USER' })
 
-export const authenticateUser = () => {
-  return {
-    type: 'AUTHENTICATE_USER'
-  }
-}
+export const authenticateUser = () => ({ type: 'AUTHENTICATE_USER' })
 
-export const receiveToken = json => {
-  return {
-    type: 'RECIEVE_TOKEN',
-    token: json.jwt
-  }
-}
+export const receiveToken = json => ({ type: 'RECIEVE_TOKEN', token: json.jwt })
+
+export const logOut = () => ({ type: 'LOGOUT' })
 
 export const createUser(user) {
   return dispatch => {
     dispatch(addUser())
     return fetch('http://localhost:3001/api/users', {
       method: 'Post',
-      body: {
-        user
-      },
-      headers: {
-        "Content-Type": "application/json"
-      }
+      body: { user },
+      headers: { "Content-Type": "application/json" }
     })
       .then(response => {
         if (response.state === 200) {
-
+          return response.json()
         }
-        response.json()
       })
   }
 }
@@ -45,14 +29,12 @@ export const fetchToken(user) {
     dispatch(authenticateUser())
     return fetch('http://localhost:3001/api/user_token', {
       method: 'POST',
-      body: {
-        auth: user
-      },
-      headers: {
-        "Content-Type": "application/json"
-      }
+      body: { auth: user },
+      headers: { "Content-Type": "application/json" }
     })
-      .then(response => response.json())
-      .then(json => json.jwt ? dispatch(receiveToken(json)) : dispatch(failedAuthentication()))
+      .then(currentUser => {
+        const { jwt } = token
+        localStorage.setItem('token', JSON.stringify(token))
+      })
   }
 }
---
+++ src/reducers/users.js
@@ -0,0 +1,21 @@
+const users = (state = {
+  token: null,
+  isAuthenticating: false,
+  isAuthenticated: false,
+  isAddingUser: false
+}, action) => {
+  switch(action.type) {
+    case 'ADD_USER':
+      return {...state, isAddingUser: true}
+    case 'AUTHENTICATE_USER':
+      return {...state, isAuthenticating: true}
+    case 'RECIEVE_TOKEN':
+      return {...state, isAuthenticating: false, isAuthenticated: true, token: action.token}
+    case 'LOGOUT':
+      return {token: null, isAuthenticated: false}
+    default:
+      return state
+  }
+}
+
+export default user
