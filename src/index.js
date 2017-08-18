78801f5d6894eb9e38b2e33ff58c39f4bee469c3
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-17 20:11:39 -0400

    Include configured store.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import configureStore from './configureStore'
 import App from './containers/App'
-import TodoApp from './reducers/index'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(TodoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
