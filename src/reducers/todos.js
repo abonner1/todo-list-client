13d2ad3366757050c3f0e905283c24be98ca39f3
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-21 17:42:46 -0400

    Add todo toggle in reducer.

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -2,8 +2,10 @@ let i = 1
 const todos = (state = [], action) => {
   switch(action.type) {
     case 'ADD_TODO':
-      const newTodo = {...action.todo, id: i++}
+      const newTodo = {...action.todo, id: i++, completed: false}
       return [...state, newTodo]
+    case 'TOGGLE_TODO':
+      return state.map(todo => (todo.id === action.id) ? {...todo, completed: !todo.completed} : todo)
     case 'DELETE_TODO':
       return state.filter(todo => todo.id !== action.id)
     default:
