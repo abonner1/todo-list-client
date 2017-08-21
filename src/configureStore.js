88b15a95548114b473791e8ccc1f8d6ab4284542
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-21 15:08:16 -0400

    Load persisted state when browser is refreshed.

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,10 +2,14 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/index'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { loadState } from './localStorage'
+
+const persistedState = loadState()
 
 const configureStore = () => {
   return createStore(
     rootReducer,
+    persistedState,
     composeWithDevTools(applyMiddleware(thunk))
   )
 }
