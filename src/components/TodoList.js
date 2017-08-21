673ed9d9e307f318f0e6c4d06028b4709fdb5e3e
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-21 18:03:01 -0400

    Remove parseInt and refactor dispatch.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,16 +4,16 @@ import { Redirect } from 'react-router-dom'
 import Todo from './Todo'
 
 
-const TodoList = ({todoLists, todos, onTodoSubmit, match}) => {
-  const currentTodoList = todoLists.find(todoList => todoList.id === parseInt(match.params.todo_list_id, 10))
+const TodoList = ({todoLists, todos, dispatch, match}) => {
+  const currentTodoList = todoLists.find(todoList => todoList.id === match.params.todo_list_id, 10)
   const currentTodos = todos.length ? todos.filter(todo => todo.todoListId === currentTodoList.id) : null
 
   return (
     <div>
       <h1>{currentTodoList && currentTodoList.name}</h1>
-      <AddTodo onTodoSubmit={onTodoSubmit} id={match.params.todo_list_id} />
+      <AddTodo onTodoSubmit={dispatch} id={match.params.todo_list_id} />
       <ul>
-        {currentTodos && currentTodos.map((todo, i) => <Todo key={i} todo={todo} onTodoSubmit={onTodoSubmit} />)}
+        {currentTodos && currentTodos.map((todo, i) => <Todo key={i} todo={todo} onTodoSubmit={dispatch} />)}
       </ul>
       {currentTodoList ? null : <Redirect from="/todo_lists/:todo_list_id" to="/todo_lists" />}
     </div>
