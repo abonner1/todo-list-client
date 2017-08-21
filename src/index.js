add4d7606f83cea77dee4dd448c53ab608a3f58b
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-21 14:55:17 -0400

    Add root route.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './configureStore'
 import App from './containers/App'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
 const store = configureStore()
@@ -11,9 +11,7 @@ const store = configureStore()
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <div>
-        <App />
-      </div>
+      <Route path="/" component={App} />
     </Router>
   </Provider>,
   document.getElementById('root')
