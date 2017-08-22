e339a1b561ca5405f5cce2947ea48a035c3ebe0c
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-21 20:23:28 -0400

    Remove logic from TodoList rendering.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,17 +5,19 @@ import Todo from './Todo'
 
 
 const TodoList = ({todoLists, todos, dispatch, match}) => {
-  const currentTodoList = todoLists.find(todoList => todoList.id === match.params.todo_list_id, 10)
-  const currentTodos = todos.length ? todos.filter(todo => todo.todoListId === currentTodoList.id) : null
+  const currentTodoList = todoLists.find(todoList => todoList.id === match.params.todo_list_id)
+  const currentTodos = currentTodoList && todos.length ? todos.filter(todo => todo.todoListId === currentTodoList.id) : null
+  const todoDisplay = currentTodos && currentTodos.map((todo, i) => <Todo key={i} todo={todo} dispatch={dispatch} />)
+  const emptyTodoListRedirect = currentTodoList ? null : <Redirect from="/todo_lists/:todo_list_id" to="/todo_lists" />
 
   return (
     <div>
       <h1>{currentTodoList && currentTodoList.name}</h1>
-      <AddTodo onTodoSubmit={dispatch} id={match.params.todo_list_id} />
+      <AddTodo dispatch={dispatch} id={match.params.todo_list_id} />
       <ul>
-        {currentTodos && currentTodos.map((todo, i) => <Todo key={i} todo={todo} onTodoSubmit={dispatch} />)}
+        {todoDisplay}
       </ul>
-      {currentTodoList ? null : <Redirect from="/todo_lists/:todo_list_id" to="/todo_lists" />}
+      {emptyTodoListRedirect}
     </div>
   )
 }
