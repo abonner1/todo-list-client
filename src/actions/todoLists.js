8eea451cb2358694183d7d1cfa8618bbdb7fe21d
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-24 18:24:25 -0400

    Add actions for fetching todolists from API and for adding them to store.

--- src/actions/todoLists.js
+++ src/actions/todoLists.js
@@ -1,3 +1,7 @@
+export const getTodoLists = () => ({ type: 'GET_TODOLISTS' })
+
+export const storeTodoLists = (todoLists) => ({ type: 'STORE_TODOLISTS', todoLists })
+
 export const addTodoList = (todoList) => ({ type: 'ADD_TODOLIST', todoList })
 
 export const deleteTodoList = id => ({ type: 'DELETE_TODOLIST', id })
