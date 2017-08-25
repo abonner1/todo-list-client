e3340545da718f9ff14376bcaacfffd08e26ed7d
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-24 21:02:37 -0400

    Fetch todos from API.

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,5 +1,23 @@
+import fetch from 'isomorphic-fetch'
+
+const getTodos = () => ({ type: 'GET_TODOS' })
+
+const storeTodos = todos => ({ type: 'STORE_TODOS', todos })
+
 export const addTodo = todo => ({ type: 'ADD_TODO', todo })
 
 export const toggleTodo = id => ({ type: 'TOGGLE_TODO', id })
 
 export const deleteTodo = id => ({ type: 'DELETE_TODO', id })
+
+export const fetchTodos = (todo_list_id, token) => {
+  return dispatch => {
+    dispatch(getTodos())
+    return fetch(`http://localhost:3001/api/todo_lists/${todo_list_id}/todos`, {
+      method: 'GET',
+      headers: { "Authorization": token }
+    })
+      .then(response => response.json())
+      .then(json => dispatch(storeTodos(json)))
+  }
+}
