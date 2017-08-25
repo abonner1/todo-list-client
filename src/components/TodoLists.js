6603681d7a0886ce8eb740772f62c7a8d376c53e
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-24 20:27:37 -0400

    Fetch todoLists on componentWillMount.

--- src/components/TodoLists.js
+++ src/components/TodoLists.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import AddTodoList from './AddTodoList'
 import TodoListDisplay from './TodoListDisplay'
+import { fetchTodoLists } from '../actions/todoLists'
 
+class TodoLists extends Component {
+  componentWillMount() {
+    this.props.dispatch(fetchTodoLists(localStorage.getItem('token')))
+  }
 
-const TodoLists = ({match, dispatch, todoLists}) => {
-  const displayTodoLists = todoLists.map((todoList, i) => {
-    return <TodoListDisplay key={i} todoList={todoList} dispatch={dispatch}/>
-  })
+  render () {
+    const displayTodoLists = this.props.todoLists.map((todoList, i) => {
+      return <TodoListDisplay key={i} todoList={todoList} dispatch={this.props.dispatch}/>
+    })
 
-  return (
-    <div className="inline todo-lists">
-      <h2>Your Todo Lists</h2>
-      <ul className="list">
-        {displayTodoLists}
-      </ul>
-      <AddTodoList dispatch={dispatch} />
-    </div>
-  )
+    return (
+      <div className="inline todo-lists">
+        <h2>Your Todo Lists</h2>
+        <ul className="list">
+          {displayTodoLists}
+        </ul>
+        <AddTodoList dispatch={this.props.dispatch} />
+      </div>
+    )
+  }
 }
 
 export default TodoLists;
