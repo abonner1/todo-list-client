9c982231dd76b628cd9c28a387b3c7bdf094242f
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-24 20:27:58 -0400

    Fix id comparison bug.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,8 +5,8 @@ import Todo from './Todo'
 
 
 const TodoList = ({todoLists, todos, dispatch, match}) => {
-  const currentTodoList = todoLists.find(todoList => todoList.id === match.params.todo_list_id)
-  const currentTodos = currentTodoList && todos.length ? todos.filter(todo => todo.todoListId === currentTodoList.id) : null
+  const currentTodoList = todoLists.find(todoList => todoList.id === parseInt(match.params.todo_list_id, 10))
+  const currentTodos = currentTodoList && todos.length ? todos.filter(todo => todo.todoListId === parseInt(currentTodoList.id, 10)) : null
   const todoDisplay = currentTodos && currentTodos.map((todo, i) => <Todo key={i} todo={todo} dispatch={dispatch} />)
   const emptyTodoListRedirect = currentTodoList ? null : <Redirect from="/todo_lists/:todo_list_id" to="/todo_lists" />
 
