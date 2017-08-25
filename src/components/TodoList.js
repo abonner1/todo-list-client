d04c45c710d25bd6c629c94e4b0c1fc7f75034e3
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-24 21:04:44 -0400

    Turn TodoList to class Component and Fetch Todos on willMount.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AddTodo from './AddTodo';
 import { Redirect } from 'react-router-dom'
 import Todo from './Todo'
+import { fetchTodos } from '../actions/todos'
 
 
-const TodoList = ({todoLists, todos, dispatch, match}) => {
-  const currentTodoList = todoLists.find(todoList => todoList.id === parseInt(match.params.todo_list_id, 10))
-  const currentTodos = currentTodoList && todos.length ? todos.filter(todo => todo.todoListId === parseInt(currentTodoList.id, 10)) : null
-  const todoDisplay = currentTodos && currentTodos.map((todo, i) => <Todo key={i} todo={todo} dispatch={dispatch} />)
-  const emptyTodoListRedirect = currentTodoList ? null : <Redirect from="/todo_lists/:todo_list_id" to="/todo_lists" />
+class TodoList extends Component{
+  componentWillMount() {
+    this.props.dispatch(fetchTodos(this.props.match.params.todo_list_id, localStorage.getItem('token')))
+  }
 
-  return (
-    <div className="inline todo-list">
-      <h2>{currentTodoList && currentTodoList.name}</h2>
-      <AddTodo dispatch={dispatch} id={match.params.todo_list_id} />
-      <ul className="list">
-        {todoDisplay}
-      </ul>
-      {emptyTodoListRedirect}
-    </div>
-  )
+  render() {
+    const currentTodoList = this.props.todoLists.find(todoList => todoList.id === parseInt(this.props.match.params.todo_list_id, 10))
+    const currentTodos = currentTodoList && this.props.todos.length ? this.props.todos.filter(todo => todo.todo_list_id === parseInt(currentTodoList.id, 10)) : null
+    const todoDisplay = currentTodos && currentTodos.map((todo, i) => <Todo key={i} todo={todo} dispatch={this.props.dispatch} />)
+    const emptyTodoListRedirect = currentTodoList ? null : <Redirect from="/todo_lists/:todo_list_id" to="/todo_lists" />
+
+    return (
+      <div className="inline todo-list">
+        <h2>{currentTodoList && currentTodoList.name}</h2>
+        <AddTodo dispatch={this.props.dispatch} id={this.props.match.params.todo_list_id} />
+        <ul className="list">
+          {todoDisplay}
+        </ul>
+        {emptyTodoListRedirect}
+      </div>
+    )
+  }
 }
 
 export default TodoList;
