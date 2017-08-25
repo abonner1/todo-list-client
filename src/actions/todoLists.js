728abb7a984c261c53824d16b099277d00d42f8d
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-24 20:26:11 -0400

    Fetch and store todoLists from API.

--- src/actions/todoLists.js
+++ src/actions/todoLists.js
@@ -1,7 +1,19 @@
-export const getTodoLists = () => ({ type: 'GET_TODOLISTS' })
+const getTodoLists = () => ({ type: 'GET_TODOLISTS' })
 
-export const storeTodoLists = (todoLists) => ({ type: 'STORE_TODOLISTS', todoLists })
+const storeTodoLists = (todoLists) => ({ type: 'STORE_TODOLISTS', todoLists })
 
 export const addTodoList = (todoList) => ({ type: 'ADD_TODOLIST', todoList })
 
 export const deleteTodoList = id => ({ type: 'DELETE_TODOLIST', id })
+
+export const fetchTodoLists = (token) => {
+  return dispatch => {
+    dispatch(getTodoLists())
+    return fetch('http://localhost:3001/api/todo_lists', {
+      method: 'GET',
+      headers: { "Authorization": token }
+    })
+      .then(response => response.json())
+      .then(json => dispatch(storeTodoLists(json)))
+  }
+}
