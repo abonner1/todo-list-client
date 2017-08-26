9a5f04c8ef9ecf30073846af49e40570d568a0b9
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-26 12:14:19 -0400

    Refactor TodoListDisplay.

--- src/components/TodoListDisplay.js
+++ src/components/TodoListDisplay.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { destroyTodoList } from '../actions/todoLists'
+import { getToken } from '../helpers/getToken'
 
 const TodoListDisplay = ({todoList, dispatch}) => {
+  const handleTodoListDestroy = () => dispatch(destroyTodoList(todoList.id, getToken()))
+
   return (
     <li>
-      <Link className="todo-list-links" to={`/todo_lists/${todoList.id}`}>
-        {todoList.name}
-      </Link>
-      <button type="button" onClick={() => dispatch(destroyTodoList(todoList.id, localStorage.getItem('token')))}>
-        X
-      </button>
+      <Link className="todo-list-links" to={`/todo_lists/${todoList.id}`}>{todoList.name}</Link>
+      <button type="button" onClick={handleTodoListDestroy}>X</button>
     </li>
   )
 }
