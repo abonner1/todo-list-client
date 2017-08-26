5b241db18c066a9d7b9eb8b7446e425e912bbc61
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-26 11:56:44 -0400

    Refactor TodoList.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
-import AddTodo from './AddTodo';
-import { Redirect } from 'react-router-dom'
-import Todo from './Todo'
 import { fetchTodos } from '../actions/todos'
-
+import { getToken } from '../helpers/getToken'
+import AddTodo from './AddTodo';
+import Todo from './Todo'
+import { Redirect } from 'react-router-dom'
 
 class TodoList extends Component{
   componentDidMount() {
-    this.props.dispatch(fetchTodos(this.props.match.params.todo_list_id, localStorage.getItem('token')))
+    this.props.dispatch(fetchTodos(this.props.match.params.todo_list_id, getToken()))
   }
 
-  render() {
-    const currentTodoList = this.props.todoLists.find(todoList => todoList.id === parseInt(this.props.match.params.todo_list_id, 10))
-    const currentTodos = currentTodoList && this.props.todos.length ? this.props.todos.filter(todo => todo.todo_list_id === parseInt(currentTodoList.id, 10)) : null
-    const todoDisplay = currentTodos && currentTodos.map((todo, i) => <Todo key={i} todo={todo} dispatch={this.props.dispatch} />)
-    const emptyTodoListRedirect = currentTodoList ? null : <Redirect from="/todo_lists/:todo_list_id" to="/todo_lists" />
+  currentTodoList = () => {
+    let currentTodoList = this.props.todoLists.find(todoList => {
+      return todoList.id === parseInt(this.props.match.params.todo_list_id, 10)
+    })
+    return currentTodoList
+  }
 
+  currentTodos = () =>
+    (this.props.todos.length)
+    ? this.props.todos.filter(todo => {
+      return todo.todo_list_id === parseInt(this.currentTodoList().id, 10)
+    })
+    : null
+
+  todoDisplay = () => this.currentTodos().map((todo, i) => {
+    return <Todo key={i} todo={todo} dispatch={this.props.dispatch} />
+  })
+
+  emptyTodoListRedirect = () =>
+    this.currentTodoList
+    ? null
+    : <Redirect from="/todo_lists/:todo_list_id" to="/todo_lists" />
+
+  render() {
     return (
       <div className="inline todo-list">
-        <h2>{currentTodoList && currentTodoList.name}</h2>
+        <h2>{this.currentTodoList().name}</h2>
         <AddTodo dispatch={this.props.dispatch} id={this.props.match.params.todo_list_id} />
         <ul className="list">
-          {todoDisplay}
+          {this.todoDisplay()}
         </ul>
-        {emptyTodoListRedirect}
+        {this.emptyTodoListRedirect()}
       </div>
     )
   }
