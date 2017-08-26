7b00dda19051513f5308ca7c389c872c678bf0d9
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-26 11:32:53 -0400

    Add updateTodo asynchronous action to Todo click handler.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { destroyTodo } from '../actions/todos'
-import { toggleTodo } from '../actions/todos'
+import { updateTodo } from '../actions/todos'
 
 const Todo = (props) => {
+  const handleTodoToggle = () => props.dispatch(updateTodo(props.todo), localStorage.getItem('token'))
+
+  const handleTodoDestroy = () => props.dispatch(destroyTodo(props.todo.id), localStorage.getItem('token'))
+
   return (
     <li style={{textDecoration: props.todo.completed ? 'line-through' : 'none'}}>
-      <span onClick={() => props.dispatch(toggleTodo(props.todo.id))}>{props.todo.description}</span>
-      <button type="button" onClick={() => props.dispatch(destroyTodo(props.todo.id), localStorage.getItem('token'))}>
-        X
-      </button>
+      <span onClick={handleTodoToggle} >{props.todo.description}</span>
+      <button type="button" onClick={handleTodoDestroy}>Delete</button>
     </li>
   )
 }
