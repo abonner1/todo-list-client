1efdfba974230c603105b3e3e67ed535012c3e33
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-26 11:41:23 -0400

    Refactor Signin.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -28,8 +28,18 @@ class SignIn extends Component {
       <div>
         <h1>Sign in</h1>
         <form onSubmit={this.handleSubmit} >
-          <input type="text" name="email" placeholder="Your Email" onChange={this.handleChange} value={this.state.email} /><br/>
-          <input type="password" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password} /><br/>
+          <input
+            type="text"
+            name="email"
+            placeholder="Your Email"
+            onChange={this.handleChange}
+            value={this.state.email} /><br/>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={this.handleChange}
+            value={this.state.password} /><br/>
           <input type="submit" value="Sign in" />
         </form>
       </div>
