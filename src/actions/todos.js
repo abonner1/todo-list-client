c02c41e2c04a6e0c838187484e0926e2e0ce5d09
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-26 11:31:02 -0400

    Add update todo asynchronous action.

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -6,7 +6,7 @@ const storeTodos = todos => ({ type: 'STORE_TODOS', todos })
 
 const addTodo = todo => ({ type: 'ADD_TODO', todo })
 
-export const toggleTodo = id => ({ type: 'TOGGLE_TODO', id })
+const toggleTodo = id => ({ type: 'TOGGLE_TODO', id })
 
 const deleteTodo = id => ({ type: 'DELETE_TODO', id })
 
@@ -34,6 +34,22 @@ export const createTodo = (todo, id, token) => {
     })
       .then(response => response.json())
       .then(json => dispatch(addTodo(json)))
+  }
+}
+
+export const updateTodo = (todo, token) => {
+  return dispatch => {
+    let update = {...todo, completed: !todo.completed}
+    return fetch(`http://localhost:3001/api/todos/${todo.id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({todo: update}),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token
+      }
+    })
+      .then(response => response.json())
+      .then(json => dispatch(toggleTodo(json.id)))
   }
 }
 
