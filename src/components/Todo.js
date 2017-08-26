a4c982e007ed5bdd82acd5ac32d551024b6291ab
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-26 11:08:47 -0400

    Dispatch destroyTodo to delete todo.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { deleteTodo } from '../actions/todos'
+import { destroyTodo } from '../actions/todos'
 import { toggleTodo } from '../actions/todos'
 
 const Todo = (props) => {
   return (
     <li style={{textDecoration: props.todo.completed ? 'line-through' : 'none'}}>
       <span onClick={() => props.dispatch(toggleTodo(props.todo.id))}>{props.todo.description}</span>
-      <button type="button" onClick={() => props.dispatch(deleteTodo(props.todo.id))}>
+      <button type="button" onClick={() => props.dispatch(destroyTodo(props.todo.id), localStorage.getItem('token'))}>
         X
       </button>
     </li>
