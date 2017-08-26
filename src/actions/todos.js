2b1c0838ffd8e0c39e6bb6812a9ae31c873ae7cd
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-26 10:50:23 -0400

    Add createTodo call to API.

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -4,7 +4,7 @@ const getTodos = () => ({ type: 'GET_TODOS' })
 
 const storeTodos = todos => ({ type: 'STORE_TODOS', todos })
 
-export const addTodo = todo => ({ type: 'ADD_TODO', todo })
+const addTodo = todo => ({ type: 'ADD_TODO', todo })
 
 export const toggleTodo = id => ({ type: 'TOGGLE_TODO', id })
 
@@ -19,5 +19,20 @@ export const fetchTodos = (todo_list_id, token) => {
     })
       .then(response => response.json())
       .then(json => dispatch(storeTodos(json)))
+  }
+}
+
+export const createTodo = (todo, id, token) => {
+  return dispatch => {
+    return fetch(`http://localhost:3001/api/todo_lists/${id}/todos`, {
+      method: 'POST',
+      body: JSON.stringify({todo}),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token
+      }
+    })
+      .then(response => response.json())
+      .then(json => dispatch(addTodo(json)))
   }
 }
