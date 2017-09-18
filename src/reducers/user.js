b0370c10e5d01e91a000bbcf2a4f9b156346c3ce
Andrew Bonner <andrewbonner92@gmail.com>
2017-09-18 18:48:26 -0400

    Handle failed registration. NOTE: Successful registration broken.

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -2,11 +2,14 @@ const user = (state = {
   token: null,
   isAuthenticating: false,
   isAuthenticated: false,
-  isAddingUser: false
+  isAddingUser: false,
+  registrationFailed: false
 }, action) => {
   switch(action.type) {
     case 'ADD_USER':
       return {...state, isAddingUser: true}
+    case 'REGISTRATION_FAILED':
+      return {...state, registrationFailed: true, message: action.message }
     case 'AUTHENTICATE_USER':
       return {...state, isAuthenticating: true, isAddingUser: false}
     case 'RECIEVE_TOKEN':
